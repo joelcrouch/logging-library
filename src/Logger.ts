488e00eb9701e1f662b12f1b9e18a262bbb0e7f0
Joel Crouch <crouchj@pdx.edu>
2022-02-22 13:27:22 -0800

    added logger class beginning

--- src/Logger.ts
+++ src/Logger.ts
@@ -14,4 +14,37 @@
  * run. Delete this once you've defined at least one export of
  * your own in this file.
  */
+export enum StatusCode{
+    ON,
+    OFF,
+}
+export class logger {
+    constructor() {
+        this.errorLevel = StatusCode.ON;
+        this.warningLevel = StatusCode.ON;
+        this.informationalLevel = StatusCode.ON;
+        this.debugLevel = StatusCode.ON;
+    }
+    errorLevel : StatusCode;
+    warningLevel : StatusCode;
+    informationalLevel : StatusCode;
+    debugLevel : StatusCode;
+    //at instantation the logger is configured 
+    //each logger has a "write member" the write member is  a function
+    //each logger has all the levels of logging
+    //each logger can be configured to turn on/off logging
+    displayLoggingLevel(){
+        console.log(this.errorLevel);
+        console.log(this.informationalLevel);
+        console.log(this.debugLevel);
+        console.log(this.warningLevel);
+
+    }
+    config(x: number) :void {
+        if (x=1)
+            this.errorLevel = StatusCode.ON;
+        if (x=2)    
+            this.
+    }
+}
 export const dummy = 0;
